9f5daef732685c0ae48e8d144c2f65ce242bb096
earth2travis <travis@l4m.co>
2020-07-20 18:22:59 -0500

    Update styles and images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses on the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Learn to Design and Code React apps</h1>
+          <p>
+            Complete courses about the best tools and design systems. Prototype
+            and build apps with React and Swift.
+          </p>
+          <Link to="/page-2/">Watch the video</Link>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
