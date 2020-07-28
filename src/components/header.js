bbc08042bd6fb59422c7c899e47c6ee2aa3e62cd
earth2travis <travis@l4m.co>
2020-07-28 17:15:36 -0500

    Add cards, sections and wave. Update nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,57 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { Link } from "gatsby"
+import "./header.css"
 
-const Header = ({ siteTitle }) => <header></header>
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <header>
+        <div
+          className={
+            this.state.hasScrolled ? "Header HeaderScrolled" : "Header"
+          }
+        >
+          <div className="HeaderGroup">
+            <Link to="/">
+              <img
+                src={require("../images/logo-designcode.svg")}
+                alt="Design+Code Logo"
+                width="30"
+              />
+            </Link>
+            <Link to="/courses">Courses</Link>
+            <Link to="/downloads">Downloads</Link>
+            <Link to="/workshops">Workshops</Link>
+            <Link to="/buy">
+              <button>Buy</button>
+            </Link>
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Card from "../components/card"
+import Section from "../components/sections"
+import Wave from "../components/wave"
 
 const IndexPage = () => (
   <Layout>
@@ -9,14 +12,81 @@ const IndexPage = () => (
     <div>
       <div className="Hero">
         <div className="HeroGroup">
-          <h1>Learn to Design and Code React apps</h1>
+          <h1>
+            Learn to <br />
+            Design and Code React apps
+          </h1>
           <p>
             Complete courses about the best tools and design systems. Prototype
             and build apps with React and Swift.
           </p>
           <Link to="/page-2/">Watch the video</Link>
+          <div className="Logos">
+            <img
+              src={require("../images/logo-sketch.png")}
+              alt="Sketch Logo"
+              width="50"
+            />
+            <img
+              src={require("../images/logo-figma.png")}
+              alt="Figma Logo"
+              width="50"
+            />
+            <img
+              src={require("../images/logo-studio.png")}
+              alt="Studio Logo"
+              width="50"
+            />
+            <img
+              src={require("../images/logo-framer.png")}
+              alt="Framer Logo"
+              width="50"
+            />
+            <img
+              src={require("../images/logo-react.png")}
+              alt="React Logo"
+              width="50"
+            />
+            <img
+              src={require("../images/logo-swift.png")}
+              alt="Swift Logo"
+              width="50"
+            />
+          </div>
+          <Wave />
         </div>
       </div>
+      <div className="Cards">
+        <h2>11 courses, more coming</h2>
+        <div className="CardGroup">
+          <Card
+            title="Design System"
+            text="10 Sections"
+            image={require("../images/wallpaper.jpg")}
+          />
+          <Card
+            title="React for Designers"
+            text="12 Sections"
+            image={require("../images/wallpaper2.jpg")}
+          />
+          <Card
+            title="Sound Designer"
+            text="5 Sections"
+            image={require("../images/wallpaper3.jpg")}
+          />
+          <Card
+            title="ARKit 2"
+            text="10 Sections"
+            image={require("../images/wallpaper4.jpg")}
+          />
+        </div>
+      </div>
+      <Section
+        image={require("../images/wallpaper2.jpg")}
+        logo={require("../images/logo-react.png")}
+        title="React for Designers"
+        text="Learn how to build a modern site using React and the most efficient libraries to get your site/ product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+      />
     </div>
   </Layout>
 )
